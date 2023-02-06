ed1f3dba21c7c726db775bebc0508f5e4ec0b726
yasmeen-abotalb <yasmeen.abotalb55@gmail.com>
2023-02-06 21:30:24 +0200

    add about page and profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ import { RegisterComponent } from './Components/register/register.component';
 import { AboutComponent } from './Components/about/about.component';
 import { ProfileComponent } from './Components/profile/profile.component';
 import { AllProductsComponent } from './Components/all-products/all-products.component';
+import { SideOrderComponent } from './Components/side-order/side-order.component';
 // import { ErrorComponent } from './Components/error/error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'profile/settings', component: ProfileComponent },
+  { path: 'profile/orders', component: SideOrderComponent },
   { path: 'login', component: LoginComponent },
   { path: 'products', component: AllProductsComponent },
   { path: 'register', component: RegisterComponent },
